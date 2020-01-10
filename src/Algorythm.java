4d0f5358e3d680fc9aa9f7fddec14317fd039366
kelgrin <kelgrin@mail.ru>
2020-01-10 22:09:18 +0600

    lab-1

---
+++ src/Algorythm.java
@@ -0,0 +1,9 @@
+class Algorythm {
+    String msg;
+    public Algorythm(){
+        msg = "Good Morning!";
+    }
+    public  void out() {
+        System.out.println(msg);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+class GoodMorning {
+    GoodMorning() { /* compiled code */
+    }
+    public static void main(java.lang.String[] strings) { /* compiled code */
+    }
+}
+
